cf519dd5868ff1bd2386ffc47fbd38903c5af2ed
Mythic Maniac <mythicmaniac@gmail.com>
2016-04-06 03:04:07 +0300

    Merge Jaymus3's repository

--- lua/champions/Nidalee/Passive.lua
+++ lua/champions/Nidalee/Passive.lua
@@ -4,5 +4,8 @@ end
 function onDamageTaken(attacker, damage, dmgType, source)
 end
 
+function onAutoAttack(target)
+end
+
 function onDealDamage(target, damage, damageType, source)
 end
--- lua/champions/Pantheon/Passive.lua
+++ lua/champions/Pantheon/Passive.lua
@@ -1,11 +1,11 @@
 function onUpdate(diff)
-    --print("Onupdate")
 end
 
 function onDamageTaken(attacker, damage, dmgType, source)
-    print("Damage taken is ".. damage)
 end
 
-function onAutoAttack(us, target)
-    print("We just auto attacked!")
+function onAutoAttack(target)
+end
+
+function onDealDamage(target, damage, damageType, source)
 end
